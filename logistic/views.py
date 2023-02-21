6d358d6a7c84b8cc2fec29e48f1f1463e3eb7054
Evgen <evgen.k.95@yandex.ru>
2023-02-21 15:47:22 +0300

    Add pagination with limit and offset

--- logistic/views.py
+++ logistic/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
@@ -9,6 +10,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [SearchFilter]
     search_fields = ['title']
+    pagination_class = LimitOffsetPagination
     
 
 class StockViewSet(ModelViewSet):
